0ed1540f21e96b0e4ab980e69ef34dfc99d83f4a
chiihero <853879993@qq.com>
2018-11-20 01:22:26 +0800

    优化部分代码

--- src/main/java/com/chii/www/service/PoiService.java
+++ src/main/java/com/chii/www/service/PoiService.java
@@ -1,17 +1,17 @@
-package com.chii.www.service;
-
-import com.chii.www.pojo.Student;
-import org.apache.poi.ss.usermodel.Cell;
-import javax.servlet.http.HttpServletResponse;
-import java.io.InputStream;
-import java.util.List;
-
-public interface PoiService {
-     void exportXLS(HttpServletResponse response);
-     List<Student> readReport(InputStream inp);
-     List<Student> getDatasource();
-     int[] insertStudent(List<Student> list);
-    Student addingStudent(int j, Student student, String cellStr);
-     String ConvertCellStr(Cell cell, String cellStr);
-
-}
+//package com.chii.www.service;
+//
+//import com.chii.www.pojo.Student;
+//import org.apache.poi.ss.usermodel.Cell;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.InputStream;
+//import java.util.List;
+//
+//public interface PoiService {
+//     void exportXLS(HttpServletResponse response);
+//     List<Student> readReport(InputStream inp);
+//     List<Student> getDatasource();
+//     int[] insertStudent(List<Student> list);
+//    Student addingStudent(int j, Student student, String cellStr);
+//     String ConvertCellStr(Cell cell, String cellStr);
+//
+//}
